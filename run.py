b89ecda056dec0bfefb700533fb45ddc64b4f4e7
Angela Anjorin <angela.anjorin@gmail.com>
2024-05-09 07:51:29 +0000

    Add main function

--- run.py
+++ run.py
@@ -61,8 +61,13 @@ def update_sales_worksheet(data):
     sales_worksheet.append_row(data)
     print("Sales worksheet updated successfully.\n")
 
+def main():
+    """
+    Run all programm functions
+    """
+    data = get_sales_data()
+    sales_data = [int(num) for num in data]
+    update_sales_worksheet(sales_data)
 
-
-data = get_sales_data()
-sales_data = [int(num) for num in data]
-update_sales_worksheet(sales_data)
+print("Welcome to Love Sandwiches Data Automation")
+main()
